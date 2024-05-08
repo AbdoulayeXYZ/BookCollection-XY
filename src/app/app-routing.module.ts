47b718d3ea23f7e5fb475fad0001e8dc52e61a13
AbdoulayeXYZ <niasseabdoulaye64@gmail.com>
2024-05-08 14:31:54 +0000

    lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/components/home/home.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { SignupComponent } from './core/components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'portal',
+    loadChildren:  () => import('./portal/portal.module').then(x => x.PortalModule),
+    data: { preload: false }
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
